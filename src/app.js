a88803fb265da4489a171e104d53f3dce46b9af9
hangnik <gkdud667485@naver.com>
2023-10-06 09:46:14 +0900

    ✅ Add : 231005 수업 정리

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import "./style.css";
 import tiger from "./tiger.png";
 
+// console.log(pw);
+
+let env;
+
+if (process.env.NODE_ENV === "development") {
+  env = dev;
+} else {
+  env = pro;
+}
+
+console.log(env);
+
 document.addEventListener("DOMContentLoaded", () => {
   document.body.innerHTML = `<img src="${tiger}"/>`;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 const webpack = require("webpack");
+const childProcess = require("child_process");
+require("dotenv").config();
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 // 모듈을 밖으로 빼내는 노드 JS 문법
 // 엔드틸, 아웃풋, 번들링 모드를 설정할 수 있다
@@ -41,7 +46,38 @@ module.exports = {
 
   plugins: [
     new webpack.BannerPlugin({
-      banner: `마지막 빌드 시간은 : ${new Date().toLocaleString()} 입니다.`,
+      banner: `
+      Commit version : ${childProcess.execSync("git rev-parse --short HEAD")}
+      Committer : ${childProcess.execSync("git config user.name")} 
+      마지막 빌드 시간 : ${new Date().toLocaleString()} .`,
     }),
+
+    new webpack.DefinePlugin({
+      //pw: 123456,
+      dev: JSON.stringify(process.env.DEV_API),
+      pro: JSON.stringify(process.env.PRO_API),
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+
+    new CleanWebpackPlugin(),
   ],
+
+  optimization: {
+    // 이미지 압축 작업을 실행할지 결정
+    minimize: true,
+    minimizer: [
+      new ImageMinimizerPlugin({
+        test: /\.(jpe?g|png|gif|svg)/i,
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [["imagemin-optipng", { optimizationLevel: 3 }]],
+          },
+        },
+      }),
+    ],
+  },
 };
